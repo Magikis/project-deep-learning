ec8c527df27392ea74fc11b8d4407202fe2edf55
Magikis <kulczakkacper@gmail.com>
2019-04-14 12:02:27 +0200

    rno env changes

--- rno-env/create_container.sh
+++ rno-env/create_container.sh
@@ -2,7 +2,7 @@
 source config.sh
 
 nvidia-docker run  --name ${USER_NAME}_${FRAMEWORK}_gpu \
-	--volume=/home/${USER_NAME}/workspace_docker:/home/${USER_NAME}/workspace_docker -it -d -p ${SSH_PORT}:4444 \
+	--volume=/home/${USER_NAME}/workspace_docker:/home/${USER_NAME}/workspace_docker -it -d -p ${SSH_PORT}:22 \
 	-p ${JUPYTER_PORT}:8888 \
 	--volume=/home/${USER_NAME}/data/datasets:/home/${USER_NAME}/data/datasets \
 	${USER_NAME}_${FRAMEWORK}
